be390bc642cd2625e3e7cabf3b4c4af08fb7074e
Guittet <og67guittetpro@gmail.com>
2024-10-09 14:12:50 +0200

    Formulaire de commit:
    Sujet : Modification Fichier liaison_bdd.py v2
    Description:
    - récupération particulières de data
    Statut: Opérationnel
    Nom/Prénom: Guittet Olivier
    Date: 09/10/2024
    Commentaires supplémentaires :

--- Python/liaison_bdd.py
+++ Python/liaison_bdd.py
@@ -22,7 +22,7 @@ def fetch_data(table_name, columns):
             data = response.json()
             # Afficher les données récupérées
             print(f"Données récupérées depuis la table {table_name} :")
-            print(json.dumps(data, indent=4))  # Affichage formaté
+            print(json.dumps(data, indent=4))  # Affichage formatée
         else:
             print(f"Erreur : {response.status_code} - {response.text}")
     except requests.exceptions.RequestException as e:
