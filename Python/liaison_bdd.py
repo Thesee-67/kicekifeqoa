f37b5ea89cb314515564f2e1e17da4133ffa31bf
Guittet <og67guittetpro@gmail.com>
2024-10-12 22:48:47 +0200

    Formulaire de commit:
    Sujet : Modification api et liason bdd
    - Mise en place recherche join
    Statut: Opérationnel
    Nom/Prénom: Guittet Olivier
    Date: 12/10/2024
    Commentaires supplémentaires :

--- Python/liaison_bdd.py
+++ Python/liaison_bdd.py
@@ -5,13 +5,18 @@ import json
 url = "http://kicekifeqoa.alwaysdata.net/api.php"
 
 
-def get_data(table, columns='*', filter_column=None, filter_value=None):
+def get_data(table, columns='*', filter_column=None, filter_value=None, join_table=None, join_condition=None):
     params = {'table': table, 'columns': columns}
 
-    # Ajouter le filtre s'il est spécifié
+    # Ajouter les filtres s'ils sont spécifiés
     if filter_column and filter_value:
         params['filter_column'] = filter_column
         params['filter_value'] = filter_value
+
+    # Ajouter la jointure si elle est spécifiée
+    if join_table and join_condition:
+        params['join_table'] = join_table
+        params['join_condition'] = join_condition
 
     response = requests.get(url, params=params)
     if response.status_code == 200:
@@ -51,7 +56,7 @@ def delete_data(table, column, value):
 
 # Exemples d'utilisation
 # 1. Récupérer toutes les colonnes d'une table
-#get_data("Users")
+#et_data("Users")
 
 # 2. Récupérer des colonnes spécifiques
 #get_data("Users", "email")
@@ -61,6 +66,10 @@ def delete_data(table, column, value):
 
 # 4. Récupérer toutes les informations liées à une valeur spécifique (par exemple, nom de personne)
 #get_data("Users", "*", filter_column="id_user", filter_value="1")
+
+
+# 5. Récupérer des données avec une jointure:
+#get_data("Task_has_Users", "Task.name,Users.email", join_table="Task,Users", join_condition="Task_has_Users.task_id = Task.id_task,Task_has_Users.user_id = Users.id_user", filter_column="Users.id_user", filter_value="1")
 
 # Ajouter des données
 #add_data("test", {"alpha": "tic","beta": "fax"})
