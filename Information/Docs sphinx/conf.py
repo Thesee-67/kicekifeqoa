b0db121183b7653ab3f985339a46bfaef5c3ee3c
Luca <monachello.luca68@gmail.com>
2024-12-19 23:44:31 +0100

    Docs Sphinx CRUD

---
+++ Information/Docs sphinx/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Docs Sphinx'
+copyright = '2024, Kicekifeqoa'
+author = 'Kicekifeqoa'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+root_doc = 'Documentation Sphinx'
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ Information/Docs sphinx/index.rst
@@ -0,0 +1,29 @@
+=====================================
+Documentation des fonctionnalités CRUD
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: CRUD
+
+   CRUD/Group/CreateGroup
+   CRUD/Group/DeleteGroup
+   CRUD/Group/ReadGroup
+   CRUD/Group/UpdateGroup
+
+   CRUD/Subtask/CreateSubtask
+   CRUD/Subtask/DeleteSubtask
+   CRUD/Subtask/ReadSubtask
+   CRUD/Subtask/UpdateSubtask
+
+   CRUD/Task/CreateTask
+   CRUD/Task/DeleteTask
+   CRUD/Task/ReadTask
+   CRUD/Task/UpdateTask
+
+   CRUD/TaskHasUsers/TaskHasUsers
+
+   CRUD/Users/CreateUser
+   CRUD/Users/DeleteUser
+   CRUD/Users/ReadUser
+   CRUD/Users/UpdateUser
